41df07ad123f2c867decbcfd7ec289de833eb1ce
kktsuji <31529355+kktsuji@users.noreply.github.com>
2025-06-23 08:40:32 +0900

    Add target module to train_lora.py

--- train_lora.py
+++ train_lora.py
@@ -1,15 +1,17 @@
 import os
 
-RESOLUTION = "40"
+# RESOLUTION = "40" # original size
+# RESOLUTION = "256"
+RESOLUTION = "512"  # recommended for sd-1.x
 
-BASE_DATA_DIR_PATH = "./data/pseudo_rgb/"
-print(BASE_DATA_DIR_PATH, os.path.exists(BASE_DATA_DIR_PATH))
-
-OUT_DIR_NAME = f"250622_cell_rois_lora_size-{RESOLUTION}"
+OUT_DIR_NAME = f"250622_cell_rois_lora_size-{RESOLUTION}"  # Edit
 OUT_DIR_PATH = f"./loras/{OUT_DIR_NAME}/"
 if not os.path.exists(OUT_DIR_PATH):
     os.makedirs(OUT_DIR_PATH)
 print(OUT_DIR_PATH, os.path.exists(OUT_DIR_PATH))
+
+BASE_DATA_DIR_PATH = "./data/pseudo_rgb/"
+print(BASE_DATA_DIR_PATH, os.path.exists(BASE_DATA_DIR_PATH))
 
 model_id = "CompVis/stable-diffusion-v1-4"
 MODEL_DIR_PATH = "./models/" + model_id
@@ -19,6 +21,35 @@ import sys
 
 sys.path.append("./peft/examples/stable_diffusion")
 import train_dreambooth
+
+print(train_dreambooth.UNET_TARGET_MODULES)
+
+# for "CompVis/stable-diffusion-v1-4"
+UNET_TARGET_MODULES = [
+    # Convolution layers
+    "conv_in",
+    "conv1",
+    "conv2",
+    "conv_out",
+    "conv_shortcut",
+    "conv",
+    # Linear layers
+    "to_q",
+    "to_k",
+    "to_v",
+    "to_out.0",
+    "linear_1",
+    "linear_2",
+    "proj_in",
+    "proj_out",
+    "proj",
+    "time_emb_proj",
+    "ff.net.0.proj",
+    "ff.net.2",
+]
+
+train_dreambooth.UNET_TARGET_MODULES = UNET_TARGET_MODULES
+print(train_dreambooth.UNET_TARGET_MODULES)
 
 input_args = [
     "--pretrained_model_name_or_path",
